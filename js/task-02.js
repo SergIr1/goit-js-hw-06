ec35531e251bb5d6015a187745153aa0f9354dde
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-05 23:26:01 +0000

    дз2 готово

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,38 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// ================Вариант 1==================
+
+// const elements = [];
+
+// for (let i = 0; i < ingredients.length; i += 1){
+//   // console.log(ingredients[i]);
+//   const nawItemsEl = document.createElement('li');
+//   nawItemsEl.classList.add('item');
+//   nawItemsEl.textContent = ingredients[i];
+
+//   elements.push(nawItemsEl);
+// }
+
+// const navItemList = document.querySelector('#ingredients');
+
+// navItemList.append(...elements);
+
+// ================Вариант 1==================
+
+const makeNavItems = items => {
+  return items.map(item => {
+    const nawItemsEl = document.createElement('li');
+    nawItemsEl.classList.add('item');
+    nawItemsEl.textContent = item;
+
+    return nawItemsEl;
+  });
+}
+
+const elements = makeNavItems(ingredients);
+
+const navItemList = document.querySelector('#ingredients');
+
+navItemList.append(...elements);
