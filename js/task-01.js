272913cc2572c6573a9b165e93fe99bd543326af
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-05 20:33:19 +0000

    дз номер 1 готово

--- js/task-01.js
+++ js/task-01.js
@@ -1,41 +1,14 @@
-// Посчитает и выведет в консоль количество категорий в ul#categories, то есть элементов li.item.
-// Для каждого элемента li.item в списке ul#categories, найдет и выведет в консоль текст заголовка элемента (тега <h2>) и количество элементов в категории (всех вложенных в него <li>).
-// В результате, в консоли будут выведены такие сообщения.
-
-// Number of categories: 3
-
-// Category: Animals
-// Elements: 4
-
-// Category: Products
-// Elements: 3
-
-// Category: Technologies
-// Elements: 5
 
 const nawEl = document.querySelectorAll('.item');
-// for (const x of nawEl) {
-//     console.log(x)
-// }
+
+const totalCatigories = `Number of categories: ${nawEl.length}`;
+
+console.log(totalCatigories);
 
 
-const numberCatigories = `Number of categories: ${nawEl.length}`;
-
-console.log(numberCatigories);
-
-
-
-const titleEl = document.querySelectorAll('h2');
-
-
+const titleEl = document.querySelectorAll('.item')
 
 titleEl.forEach(element => {
-    console.log(`Category: ${element.textContent}`);
-    // console.log(`Elements: ${element.li}`)
+    console.log(`Category: ${element.querySelector('h2').textContent}`);
+    console.log(`Elements: ${element.querySelectorAll('li').length}`);
 });
-
-// console.log(titleEl.textContent);
-
-const x = nawEl.children;
-
-console.log(x)
