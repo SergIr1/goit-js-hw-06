6ffe41c1d73f13220580b2d388ee85d10a757c84
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-06 18:25:34 +0000

    дз 3 готово

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,46 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+// ==============Вариант 1==================
+
+// const itemEl = document.createElement('li');
+// itemEl.classList.add('img-gallary');
+
+// const makeGallaryCard = options => {
+//   return options.map(({url, alt}) => {
+//     const imgEl = document.createElement('img');
+//     imgEl.width = 640;
+//     imgEl.src = url;
+//     imgEl.alt = alt;
+
+//     return imgEl;
+//   });
+// }
+
+// const elements = makeGallaryCard(images);
+
+// itemEl.append(...elements);
+// // console.log(...elements)
+
+// const itemGallary = document.querySelector('.gallery');
+
+// itemGallary.append(itemEl);
+
+// ==============Вариант 1==================
+
+const makeArrayItemGallaryMarkup = gallarys => {
+  return gallarys.map((image) => {
+    return image = `<li><img src = ${image.url} alt = ${image.alt.split(' ').join('&#32')} width=640 class = card-set__gallary></li>`;
+  }).join('');
+}
+
+const arrayGallary = makeArrayItemGallaryMarkup(images);
+  
+const itemGallary = document.querySelector('.gallery');
+itemGallary.classList.add('card-set');
+
+itemGallary.insertAdjacentHTML('afterbegin', arrayGallary);
+
+console.log(itemGallary);
+
