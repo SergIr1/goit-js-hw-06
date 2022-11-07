94c4832e63d8b81de2ce9574a1a2bc69b2c003d6
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-07 22:18:13 +0000

    дз 9 готово

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+
+const refs = {
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+};
+
+const changeBodyBgrColor = (event) => {
+  refs.body.style.backgroundColor = getRandomHexColor();
+  refs.span.textContent = getRandomHexColor();
+};
+
+refs.btn.addEventListener('click', changeBodyBgrColor);
