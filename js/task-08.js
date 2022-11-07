a58e6d2469d1b67ea77691b383a0a9c647184f22
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-07 21:37:07 +0000

    дз 8 готово

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const loginForm = document.querySelector('.login-form');
+console.log(loginForm);
 
+loginForm.addEventListener('submit', onloginFormSubmit);
+
+function onloginFormSubmit(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+    
+
+      if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть заполнены.");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+};
