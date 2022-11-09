fcde7637436848f66053ac0718e5a94d0c03b33b
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-09 20:06:27 +0000

    9 готово

--- js/task-09.js
+++ js/task-09.js
@@ -2,6 +2,11 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 };
 
+// const random_hex_color_code = () => {
+//   let n = (Math.random() * 0xfffff * 1000000).toString(16);
+//   return '#' + n.slice(0, 6);
+// };
+
 const refs = {
   body: document.querySelector('body'),
   span: document.querySelector('.color'),
@@ -9,9 +14,20 @@ const refs = {
 };
 
 const changeBodyBgrColor = (event) => {
-  refs.body.style.backgroundColor = getRandomHexColor(event);
-  refs.span.textContent = getRandomHexColor(event);
+  const color = getRandomHexColor();
+  refs.body.style.backgroundColor = color;
+  refs.span.textContent = color;
 
+  // const color = refs.body.style.backgroundColor;
+
+// function ColorToHex(color) {
+//   var hexadecimal = color.toString(16);
+//   return hexadecimal.length == 1 ? "0" + hexadecimal : hexadecimal;
+// }
+
+// function ConvertRGBtoHex(red, green, blue) {
+//   return "#" + ColorToHex(red) + ColorToHex(green) + ColorToHex(blue);
+// }
 };
 
 refs.btn.addEventListener('click', changeBodyBgrColor);
