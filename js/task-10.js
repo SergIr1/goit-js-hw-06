96aa7729a961ec7fd7d7afec3c6f8a6af0850a28
Serhii Karskyi <serhiipraktic7@gmail.com>
2022-11-14 21:52:16 +0000

    1

--- js/task-10.js
+++ js/task-10.js
@@ -13,25 +13,42 @@ let step = 10;
 let width = 30;
 let height = 30;
 
+// const render = () => {
+//     refs.value.textContent = counterValue;
+// };
+
+// const onDecrementClick = () => {
+//     counterValue -= 1;
+//     render();
+// };
+
+// const onIncrementClick = () => {
+//     counterValue += 1;
+//     render();
+// };
+
+// render();
+
 const createBoxes = (amount) => {
   
-  amount = refs.input.value;
-
+  amount = refs.input.attributes.step.value;
   for (let i = 0; i < amount; i += 1){
+  console.log(amount)
+  }
+
+
+
     const newDivEL = document.createElement('div');
-    if (i = 1) {
-    newDivEL.style.width = `${width +=amount * step}px`;
+  for (let i = 0; i < amount; i += 1) {
+    newDivEL.style.width = `${width += amount * step}px`;
     newDivEL.style.height = `${height += amount * step}px`;
-    } else if (i = 0) {
-          newDivEL.style.width = `${width -=amount * step}px`;
-    newDivEL.style.height = `${height -= amount * step}px`;
-    }
+  }
     newDivEL.style.backgroundColor = getRandomHexColor();
   
     const marcupEl = refs.div.append(newDivEL);
  
   return marcupEl;
-  }
+
 };
 
 const destroyBoxes = (marcupEl = '') => {
